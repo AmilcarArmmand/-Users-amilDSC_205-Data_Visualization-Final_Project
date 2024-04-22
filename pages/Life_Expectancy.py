dd1f7ae6b49f2f5222218e65f30d74c083fefd36
Amilcar Armmand <armmand@gmail.com>
2024-04-22 18:58:12 -0400

    updated Life_Exp

--- pages/Life_Expectancy.py
+++ pages/Life_Expectancy.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
+import matplotlib.pyplot as plt
 from os import path
 
 
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Life Expectancy", page_icon="📈", layout="wide"
 
 st.markdown("# Life Expectancy From Birth")
 st.sidebar.header("Life Expectancy")
-st.write("""Include periods in the lines to indicate life expectancy in a given year.
+st.write("""
 (Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)""")
 
 
@@ -19,7 +19,7 @@ def get_data():
     URL = 'data/life-expectancy.csv'
     df = pd.read_csv(URL)
     df.rename(columns={'Period life expectancy at birth - Sex: all - Age: 0': 'life_expectancy'},
-          inplace=True)
+              inplace=True)
     return df.set_index("Entity")
 
 
@@ -27,36 +27,32 @@ try:
     df = get_data()
     data = df.loc[:, ['Year', 'life_expectancy']]
     country_options = data.index.unique()
-    #st.table(data=data)
+
     countries = st.multiselect("Choose countries", country_options, ["China", "United States"])
     if not countries:
         st.error("Please select at least one country.")
     else:
         # Filter data for a specific country, for example "United States"
-        filtered_data = data[data.index == countries]
+        country_data = {}
+
+        # Loop through the selected countries and extract the data.
+        for country in countries:
+            country_data[country] = data.loc[country]
+
+        fig = plt.figure()
+        ax = fig.add_subplot()
+
+        # Create a line plot for each country.
+        for country, data in country_data.items():
+            ax.plot(data['Year'], data['life_expectancy'], label=country)
 
         # Create a line chart
-        #chart = alt.Chart(filtered_data).mark_line().encode(
-        #    x='Year:Q',  # Quantitative scale for Year
-        #    y='life_expectancy:Q',  # Quantitative scale for life expectancy
-        #    tooltip=['Year', 'life_expectancy']  # Show tooltip on hover
-        #).properties(
-        #    title='Life Expectancy Over the Years in the United States',
-        #    width=600,
-        #    height=400
-        #)
+        ax.set_title('Life')
+        ax.set_xlabel('Year')
+        ax.set_ylabel('Age')
 
         # Display the chart
-        
-        #data = df.loc[countries]
-        #data /= 1000000.0
-        #st.write("### Gross Agricultural Production ($B)", data.sort_index())
+        st.pyplot(fig)
 
-        #data = data.T.reset_index()
-        #data = pd.melt(data, id_vars=["index"]).rename(columns={"index": "Year", "value": "life_expectancy"})
-        #chart = (alt.Chart(data).mark_area(opacity=0.3).encode(x="Year:T",                                                               y=alt.Y("Life Expectancy ($B):Q", stack=None), color="Region:N",   ))
-
-        #st.altair_chart(chart, use_container_width=True)
 except ValueError:
     st.write('Oops')
-    
