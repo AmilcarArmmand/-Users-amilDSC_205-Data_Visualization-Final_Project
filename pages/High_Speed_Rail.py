008b30029e3b385bfd2428e2bb7cfcf2593be47b
Amilcar Armmand <armmand@gmail.com>
2024-04-23 15:53:07 -0400

    updated save point for Poverty, Power, and HSR

--- pages/High_Speed_Rail.py
+++ pages/High_Speed_Rail.py
@@ -5,28 +5,38 @@ from os import path
 
 
 # Set up the page layout to use the full width of the web page
-st.set_page_config(page_title="Life Expectancy", page_icon="📈", layout="wide")
+st.set_page_config(page_title="HSP", page_icon="📈", layout="wide")
 
-st.markdown("# Power Generaltion")
-st.sidebar.header("Power Generation")
+st.markdown("# High Speed Rail")
+st.sidebar.header("High Speed Rail")
 
 
 @st.cache_data
-def get_data():
-    """        """
-    URL = 'data/energy-consumption-by-source-and-country.csv'
-    df = pd.read_csv(URL)
-    return df
+def get_data(URL):
+    """Get data"""
+    return pd.read_csv(URL)
+
+
+URL1 = 'data/HSR.csv'
+URL2 = 'data/railways-passengers-carried-passenger-km.csv'
 
 
 try:
-    df = get_data()
+    df_rail = get_data(URL1)
+    df_rider = get_data(URL2)
 
-except Error:
+    
+except TypeError:
     st.write('break point')
 
 st.subheader('st.dataframe, width = 600px, height = 200px')
-st.dataframe(df, width=600, height=200)
+st.dataframe(df_rail, width=600, height=200)
 
 st.subheader('st.table shows the contents of entire DataFrame')
-st.table(data=df)
+st.table(data=df_rail)
+
+st.subheader('st.dataframe, width = 600px, height = 200px')
+st.dataframe(df_rider, width=600, height=200)
+
+st.subheader('st.table shows the contents of entire DataFrame')
+st.table(data=df_rider)
---
+++ pages/Poverty.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from os import path
+
+
+# Set up the page layout to use the full width of the web page
+st.set_page_config(page_title="Extreme Poverty", page_icon="📈", layout="wide")
+
+st.markdown("# Extreme Poverty")
+st.sidebar.header("Extreme Poverty")
+st.write("""
+(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)""")
+
+
+@st.cache_data
+def get_data():
+    """        """
+    URL = 'data/poverty.csv'
+    df = pd.read_csv(URL)
+    df.rename(columns={'$2.15 a day - Share of population in poverty':'poverty'},
+              inplace=True)
+    return df.set_index("Entity")
+
+
+try:
+    df = get_data()
+    data = df.loc[:, ['Year', 'poverty']]
+    country_options = data.index.unique()
+
+    countries = st.multiselect("Choose countries", country_options, ["China", "United States"])
+    if not countries:
+        st.error("Please select at least one country.")
+    else:
+        # Filter data for a specific country, for example "United States"
+        country_data = {}
+
+        # Loop through the selected countries and extract the data.
+        for country in countries:
+            country_data[country] = data.loc[country]
+
+        fig = plt.figure()
+        ax = fig.add_subplot()
+
+        # Create a line plot for each country.
+        for country, data in country_data.items():
+            ax.plot(data['Year'], data['poverty'], label=country)
+
+        # Create a line chart
+        ax.set_title('Poverty')
+        ax.set_xlabel('Year')
+        ax.set_ylabel('Share of Population')
+
+        # Display the chart
+        st.pyplot(fig)
+
+except ValueError:
+    st.write('Oops')
