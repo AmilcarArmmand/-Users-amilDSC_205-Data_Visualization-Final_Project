0c6de5c6a8d0a82596764de3e230e30c132082e7
Amilcar Armmand <armmand@gmail.com>
2024-04-26 23:25:15 -0400

    save point wtih containers on homescreen

--- console.py
+++ console.py
@@ -1,9 +1,15 @@
 import streamlit as st
+import numpy as np
 
 
 st.set_page_config(
-    page_title="Frontpage",
+    page_title="Ex-stream-ly Cool App",
     page_icon="👋",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items={
+        'About': "## DSC-205 Final Project by Conner Broderick and Amilcar Armmand. This is an *extremely* cool app!"
+    }
 )
 
 st.write("# The Modernization of China :cn:")
@@ -12,8 +18,80 @@ st.sidebar.success("Select a demo above.")
 
 st.markdown(
     """
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
+    **👈 Select a page from the sidebar**
     
 """
 )
+st.divider()
+
+with st.sidebar:
+    if st.button("Home"):
+        st.switch_page("console.py")
+    if st.button("Life Expectancy"):
+        st.switch_page("pages/Life_Expectancy.py")
+    if st.button("Growth Comparison"):
+        st.switch_page("pages/Growth_Comparison.py")
+    if st.button("High Speed Rail"):
+        st.switch_page("pages/High_Speed_Rail.py")
+    if st.button("Imports and Exports"):
+        st.switch_page("pages/Imports and Exports.py")
+    if st.button("Extreme Poverty"):
+        st.switch_page("pages/Poverty.py")
+    if st.button("Power Generation"):
+        st.switch_page("pages/Power_Generation.py")
+    if st.button("Renewable Power"):
+        st.switch_page("pages/Renewable_Power.py")
+    if st.button("Agr"):
+        st.switch_page("pages/3_📊_DataFrame.py")
+    if st.button("Inspiration"):
+        st.switch_page("pages/3_📊_DataFrame.py")
+    if st.button("Trivia"):
+        st.switch_page("pages/3_📊_DataFrame.py")
+    if st.button("Photo Gallery"):
+        st.switch_page("pages/Session_State_Basics.py")
+
+with st.container():
+    tab1, tab2, tab3, tab4 = st.tabs(["Menu items", "📈 Chart", "📈 Chart", "🗃 Data"])
+
+    with tab1:
+        if st.button("_Home"):
+            st.switch_page("console.py")
+        if st.button("Life_Expectancy"):
+             st.switch_page("pages/Life_Expectancy.py")
+        if st.button("Growth_Comparison"):
+            st.switch_page("pages/Growth_Comparison.py")
+        if st.button("High_Speed_Rail"):
+            st.switch_page("pages/High_Speed_Rail.py")
+        if st.button("Imports_and_Exports"):
+            st.switch_page("pages/Imports and Exports.py")
+        if st.button("Extreme_Poverty"):
+            st.switch_page("pages/Poverty.py")
+        if st.button("Power_Generation"):
+            st.switch_page("pages/Power_Generation.py")
+        if st.button("Renewable_Power"):
+            st.switch_page("pages/Renewable_Power.py")
+        if st.button("Agro"):
+            st.switch_page("pages/3_📊_DataFrame.py")
+        if st.button("_Inspiration"):
+            st.switch_page("pages/3_📊_DataFrame.py")
+        if st.button("_Trivia"):
+            st.switch_page("pages/3_📊_DataFrame.py")
+        if st.button("Photo_Gallery"):
+            st.switch_page("pages/Session_State_Basics.py")
+    with tab2:
+    
+            st.header("tab2")
+            st.write("This is inside the container")
+
+            # You can call any Streamlit command, including custom components:
+            st.bar_chart(np.random.randn(50, 3))
+
+st.divider()
+
+c = st.container()
+st.write("This will show last. Does this expand to the full extent of the container?  Let's seeeeeeeeeeeeeeeeeeee.")
+st.bar_chart(np.random.randn(50, 3))
+c.write("This will show first")
+c.write("This will show second")
+# Code to create the link button
+st.link_button("Go to project repo.", "https://github.com/AmilcarArmmand/DSC_205-Data_Visualization-Final_Project")
--- pages/High_Speed_Rail.py
+++ pages/High_Speed_Rail.py
@@ -8,8 +8,10 @@ from os import path
 st.set_page_config(page_title="HSP", page_icon="📈", layout="wide")
 
 st.markdown("# High Speed Rail")
-st.sidebar.header("High Speed Rail")
+st.sidebar.header(" High Speed Rail")
 
+
+tab1, tab2, tab3 = st.tabs(["📈 Chart", "📈 Chart", "🗃 Data"])
 
 @st.cache_data
 def get_data(URL):
@@ -17,26 +19,44 @@ def get_data(URL):
     return pd.read_csv(URL)
 
 
-URL1 = 'data/HSR.csv'
-URL2 = 'data/railways-passengers-carried-passenger-km.csv'
-
+URL_hsr = 'data/HSR.csv'
+URL_passenger = 'data/railways-passengers-carried-passenger-km.csv'
 
 try:
-    df_rail = get_data(URL1)
-    df_rider = get_data(URL2)
+    data = get_data(URL_hsr)
+    df_rail = data.rename(columns={'Unnamed: 0': 'year',
+                             'Length\u00a0of\u00a0High\u00a0Speed\u00a0Railway\u00a0in\u00a0Operation\u00a0(km)': 'high_speed_rail_length',
+                             'As\u00a0Percentage\u00a0of\u00a0Total\u00a0Railway\u00a0Length\u00a0(%)': 'high_speed_rail_percentage',
+                             'Passenger\u00a0Traffic\u00a0(10,000\u00a0persons)': 'passenger_traffic',
+                             'As\u00a0Percentage\u00a0of\u00a0Total\u00a0Railway\u00a0Traffic\u00a0(%)': 'percent_total_rail_traffic',
+                             'Passenger-Kilometers\u00a0(100\u00a0million\u00a0person-km)': 'passengers_per_km',
+                             'As\u00a0Percentage\u00a0of\u00a0Total\u00a0Railway\u00a0Passenger-Kilometers\u00a0(%)': 'percent_total_railway_passPerKm'})
+
+    rail_data = df_rail.set_index('year')
+
+    df_passenger = get_data(URL_passenger)
+    passenger_data = df_passenger.index.unique()
+
+    with st.container():
+        with tab1:
+            st.subheader("Total km of rail.")
+            st.bar_chart(rail_data)
+
+        with tab2:
+            st.subheader("2")
+            st.line_chart(passenger_data)
+
+    tab3.subheader("Data")
+    tab3.write(passenger_data)
 
     
 except TypeError:
+    st.error('This is an error', icon="🚨")
     st.write('break point')
 
-st.subheader('st.dataframe, width = 600px, height = 200px')
-st.dataframe(df_rail, width=600, height=200)
-
-st.subheader('st.table shows the contents of entire DataFrame')
-st.table(data=df_rail)
 
 st.subheader('st.dataframe, width = 600px, height = 200px')
-st.dataframe(df_rider, width=600, height=200)
+st.dataframe(passenger_data, width=600, height=200)
 
 st.subheader('st.table shows the contents of entire DataFrame')
-st.table(data=df_rider)
+st.table(data=passenger_data)
