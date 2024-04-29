d3548185ce52d87238e802ad115edbe1a8084747
Amilcar Armmand <armmand@gmail.com>
2024-04-29 15:20:33 -0400

    added trivia questions

--- pages/media.py
+++ pages/media.py
@@ -9,52 +9,20 @@ st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
 
 st.markdown("# DataFrame Demo")
 st.sidebar.header("DataFrame Demo")
-st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-)
 
 
-@st.cache_data
-def get_UN_data():
-    AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-    return df.set_index("Region")
 
 
-try:
-    df = get_UN_data()
-    countries = st.multiselect(
-        "Choose countries", list(df.index), ["China", "United States of America"]
-    )
-    if not countries:
-        st.error("Please select at least one country.")
-    else:
-        data = df.loc[countries]
-        data /= 1000000.0
-        st.write("### Gross Agricultural Production ($B)", data.sort_index())
-
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=["index"]).rename(
-            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        )
-        chart = (
-            alt.Chart(data)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="year:T",
-                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                color="Region:N",
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e.reason
-    )
+st.markdown("""Grain output 700M tons is 1.2 times US production.\n
+•  Power generation of 9.2T kw is 2.3 times more than the U.S.\n
+•  China produces 19 times more steel than the U.S.\n
+•  China produces 20 times more cement than the U.S. \n
+•  Ship building produces 70 times more tonnage a year \n
+•  China has the 2nd largest merchant fleet (Greece is #1) \n
+•  Transportation efficiency of rail, airport, and ports. \n
+•  Installed solar power is twice all other countries combined \n
+•  The Wind power generates more power than the next 7 countries combined \n
+•  In 2020, China graduated 3.57M STEM workers / the U.S. graduated 820k(about 1/3 foreign born) \n
+•  Dominated the international Olympiads in biology, chemistry, physics, math, and informatics.""")
 
 menu()
